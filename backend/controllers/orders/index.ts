fcdd4212fa37f143c9345d1089c895e4c68fade2
krtky1993 <krtky1993@gmail.com>
2024-09-19 15:03:57 -0400

    added exception handling

--- backend/controllers/orders/index.ts
+++ backend/controllers/orders/index.ts
@@ -5,114 +5,141 @@ import products from "../../models/products.json";
 import { Product } from "../../models/types";
 
 const searchOrders: RequestHandler = (request: Request, response: Response) => {
-	const { id, email, date } = request.query;
-	let filteredOrders = orders;
-	if (id) {
-		filteredOrders = filteredOrders.filter((order) => order.orderId === id);
+	try {
+		const { id, email, date } = request.query;
+		let filteredOrders = orders;
+		if (id) {
+			filteredOrders = filteredOrders.filter((order) => order.orderId === id);
+		}
+		if (email) {
+			filteredOrders = filteredOrders.filter(
+				(order) => order.customerEmail === email
+			);
+		}
+		if (date) {
+			filteredOrders = filteredOrders.filter(
+				(order) => order.orderDate === date
+			);
+		}
+		if (filteredOrders.length) {
+			return response
+				.status(200)
+				.json({ total: filteredOrders.length, orders: filteredOrders });
+		}
+		return response.status(404).json({
+			message: "No Orders Found",
+		});
+	} catch (error) {
+		return response.status(500).json({
+			message: `Server Error Encountered: ${error}`,
+		});
 	}
-	if (email) {
-		filteredOrders = filteredOrders.filter(
-			(order) => order.customerEmail === email
-		);
-	}
-	if (date) {
-		filteredOrders = filteredOrders.filter((order) => order.orderDate === date);
-	}
-	if (filteredOrders.length) {
-		return response
-			.status(200)
-			.json({ total: filteredOrders.length, orders: filteredOrders });
-	}
-	return response.status(404).json({
-		message: "No Orders Found",
-	});
 };
 
 const getOrder = (request: Request, response: Response) => {
-	const order = orders.filter((order) => order.orderId === request.params.id);
-	if (order.length) {
-		return response.status(200).json({ total: order.length, orders: order });
+	try {
+		const order = orders.filter((order) => order.orderId === request.params.id);
+		if (order.length) {
+			return response.status(200).json({ total: order.length, orders: order });
+		}
+		return response.status(404).json({
+			message: "Order Not Found",
+		});
+	} catch (error) {
+		return response.status(500).json({
+			message: `Server Error Encountered: ${error}`,
+		});
 	}
-	return response.status(404).json({
-		message: "Order Not Found",
-	});
 };
 
 const getItemsForPicking = (request: Request, response: Response) => {
-	const { date } = request.query;
-	const filteredOrders = date
-		? orders.filter((order) => order.orderDate === date)
-		: orders;
+	try {
+		const { date } = request.query;
+		const filteredOrders = date
+			? orders.filter((order) => order.orderDate === date)
+			: orders;
 
-	const items: { [key: string]: Product } = {};
+		const items: { [key: string]: Product } = {};
 
-	filteredOrders.forEach((order) => {
-		order.lineItems.forEach((item: { productId: string }) => {
-			const product = products.find(
-				(product) => product.productId === item.productId
-			);
+		filteredOrders.forEach((order) => {
+			order.lineItems.forEach((item: { productId: string }) => {
+				const product = products.find(
+					(product) => product.productId === item.productId
+				);
 
-			if (product && product.items) {
-				product.items.forEach((subItem) => {
-					if (items[subItem.sku]) {
-						items[subItem.sku].qty! += 1;
-					} else {
-						items[subItem.sku] = {
-							sku: subItem.sku,
-							itemName: subItem.itemName,
-							price: subItem.price,
-							qty: 1,
-						};
-					}
-				});
-			}
+				if (product && product.items) {
+					product.items.forEach((subItem) => {
+						if (items[subItem.sku]) {
+							items[subItem.sku].qty! += 1;
+						} else {
+							items[subItem.sku] = {
+								sku: subItem.sku,
+								itemName: subItem.itemName,
+								price: subItem.price,
+								qty: 1,
+							};
+						}
+					});
+				}
+			});
 		});
-	});
 
-	return response.status(200).json({
-		orderDate: date,
-		totalOrders: filteredOrders.length,
-		products: Object.values(items),
-	});
+		return response.status(200).json({
+			orderDate: date,
+			totalOrders: filteredOrders.length,
+			products: Object.values(items),
+		});
+	} catch (error) {
+		return response.status(500).json({
+			message: `Server Error Encountered: ${error}`,
+		});
+	}
 };
 
 const getItemsForPacking = (request: Request, response: Response) => {
-	const { date } = request.query;
-	const filteredOrders = date
-		? orders.filter((order) => order.orderDate === date)
-		: orders;
+	try {
+		const { date } = request.query;
+		const filteredOrders = date
+			? orders.filter((order) => order.orderDate === date)
+			: orders;
 
-	const orderDetails = filteredOrders.map((order) => {
-		const {
-			orderId,
-			orderTotal,
-			orderDate,
-			shippingAddress,
-			customerEmail,
-			customerName,
-		} = order;
-		return {
-			orderId,
-			orderTotal,
-			orderDate,
-			shippingAddress,
-			customerEmail,
-			customerName,
-			lineItems: order.lineItems.map((item) => ({
-				productId: item.productId,
-				productName: item.productName,
-				qty: item.qty,
-				price: item.price,
-				items: products.find((product) => product.productId === item.productId)!
-					.items,
-			})),
-		};
-	});
+		const orderDetails = filteredOrders.map((order) => {
+			const {
+				orderId,
+				orderTotal,
+				orderDate,
+				shippingAddress,
+				customerEmail,
+				customerName,
+			} = order;
+			return {
+				orderId,
+				orderTotal,
+				orderDate,
+				shippingAddress,
+				customerEmail,
+				customerName,
+				lineItems: order.lineItems.map((item) => ({
+					productId: item.productId,
+					productName: item.productName,
+					qty: item.qty,
+					price: item.price,
+					items: products.find(
+						(product) => product.productId === item.productId
+					)!.items,
+				})),
+			};
+		});
 
-	return response.status(200).json({
-		totalOrders: orderDetails.length,
-		orders: orderDetails,
-	});
+		return response.status(200).json({
+			totalOrders: orderDetails.length,
+			orders: orderDetails,
+		});
+	} catch (error) {
+		return response.status(500).json({
+			message: `Server Error Encountered: ${error}`,
+		});
+	}
 };
 
 export { searchOrders, getOrder, getItemsForPicking, getItemsForPacking };
--- backend/controllers/products/index.ts
+++ backend/controllers/products/index.ts
@@ -3,19 +3,31 @@ import { Request, Response } from "express";
 import products from "../../models/products.json";
 
 const searchProducts = (request: Request, response: Response) => {
-	return response.status(200).json({ total: products.length, products });
+	try {
+		return response.status(200).json({ total: products.length, products });
+	} catch (error) {
+		return response.status(500).json({
+			message: `Server Error Encountered: ${error}`,
+		});
+	}
 };
 
 const getProduct = (request: Request, response: Response) => {
-	const product = products.filter((x) => x.productId === request.params.id);
-	if (product.length) {
-		return response
-			.status(200)
-			.json({ total: product.length, products: product });
+	try {
+		const product = products.filter((x) => x.productId === request.params.id);
+		if (product.length) {
+			return response
+				.status(200)
+				.json({ total: product.length, products: product });
+		}
+		return response.status(404).json({
+			message: "Product Not Found!",
+		});
+	} catch (error) {
+		return response.status(500).json({
+			message: `Server Error Encountered: ${error}`,
+		});
 	}
-	return response.status(404).json({
-		message: "Product Not Found!",
-	});
 };
 
 export { searchProducts, getProduct };
